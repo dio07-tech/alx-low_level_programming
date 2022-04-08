c6e7a123042f0c3a366ce6db5bd482fb8440882a
dio07-tech <divineikechukwu65@gmail.com>
2022-04-08 05:49:35 -0700

    updated README.md

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - programming is following set of instructions to run your code
+ * Return: gives you feedback on the result of your code
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
