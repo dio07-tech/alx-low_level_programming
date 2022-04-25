1829b37bb97e0ac1147ec0aeef1a770308318930
dio07-tech <divineikechukwu65@gmail.com>
2022-04-24 21:32:53 -0700

    more task

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,20 +1,26 @@
 #include "main.h"
 /**
- * _strcat - concats the string
+ * _strncat - concats the string
  * @dest: params
  * @src: params
+ * @n: params
  * Return: char
  */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int len = 0;
 	int len2 = 0;
 
 	while (dest[len] != '\0')
 		len++;
-	while (src[len2] != '\0')
+	while (len2 != n)
 	{
+		if (src[len2] == '\0')
+		{
+
+			break;
+		}
 		*(dest + len) = src[len2];
 		len++;
 		len2++;
